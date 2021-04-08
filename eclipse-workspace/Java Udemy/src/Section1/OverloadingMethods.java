1426fd49ae9e300dc536e6c00bd5126181a8bc70
admin <ismailozkan93@icloud.com>
2021-04-08 07:01:02 +0200

    # WARNING: head commit changed in the meantime
    first

--- eclipse-workspace/Java Udemy/src/Section1/OverloadingMethods.java
+++ eclipse-workspace/Java Udemy/src/Section1/OverloadingMethods.java
@@ -2,4 +2,47 @@ package Section1;
 
 public class OverloadingMethods {
 
+public static void main(String[] args) {
+		
 }
+
+		public static int max(int num1, int num2) {
+			if(num1>num2)
+				return num1;
+			else
+				return num2;
+			
+		}
+		public static double max(double num1, double num2) {
+			if(num1>num2)
+				return num1;
+			else
+				return num2;
+			
+		}
+		public static double max(double num1, double num2, double num3) {
+			return max(max(num1,num2),num3);
+			
+		}
+	}
+
+/*A person who can change variable type, so person can produce many methods with same "METHODNAME".It means
+ * "METHOD OVERLOADING". 
+ *A Person can make Overlaoding only with parameters.
+ *Changing return type is not enough to make "OVERLOADING."
+ *Changing Access Modifier is not enough to make "OVERLOADING"
+ *Changing Methody Body is not enough to make "OVERLOADING"
+ *METHOD OVERLOADING is COMPILE TIME POLYMORPHISM. 
+ *
+ *
+ */
+
+
+/*1)Return Type lar degistirilerek method overloading yapilmaz.Yani;ismi ve paremetreleri ayni olan iki method
+ * return typelari farkli bile olsa java onlari ayni kabul eder.
+ *2)Access Modifier lar degistirilerek method overloading yapilmaz. Yani; ismi ve parametreleri ayni olan iki metod
+ *Acess Modiefierlari farkli bile olsa Java onlari ayni kabul eder.
+ *3)Method Bodyler degistirilerek method overloading yapilmaz.Yani; ismi ve parametreleri ayni bile olsa Java onlari
+ *ayni metod kabul eder. 
+ */
+//Note: Method Overloading Compile Time Polymorpfisim dir.
--- eclipse-workspace/Java Udemy/src/Section1/PassByValue.java
+++ eclipse-workspace/Java Udemy/src/Section1/PassByValue.java
@@ -1,5 +1,23 @@
 package Section1;
 
-public class PassByValue {
 
+public class PassByValue {
+		static int x=5;//It is changed as "static".Because it has used in static method.
+		//static y;Instance variable takes default value.
+	public static void main(String[] args) {//Here is static,because of this instance variable as 
+		System.out.println(x);				//"static" changed.
+		int x=1;//Local variable'dir.It uses in method body.
+		System.out.println("Before the call,x is " + x);
+		increment(x);//Here is static because of static condition the method "increment" should be static.
+		System.out.println("After the call,x is" + x);
+
+	}
+	public static void increment(int n) {
+		//int a; it doesnt make error
+		//	System.out.println(a); //It takes error beacuse LOCAL variable doesnt take default value.
+		System.out.println(n);
+		n++;
+		System.out.println("n inside the method is"+ n);
+		
+	}
 }
